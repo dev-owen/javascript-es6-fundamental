cb005852428eb06372791f5d83f5fc832df91f2b
dev-owen <jgyuity1289@gmail.com>
2023-04-12 00:05:04 +0900

    1-3 part2 operators start

---
+++ part2/app2.js
@@ -0,0 +1,11 @@
+// Operator
+
+// 할당 연산자 (Assignment Operators)
+
+// 비교 연산자 (Comparison Operators)
+
+// 산술 연산자 (Arithmetic Operators)
+
+// 논리 연산자 (Logical Operators)
+
+// 삼항 연산자 (Conditional Operators)
