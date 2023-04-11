f15100c5a20fa3b1bb4d3112324c40fc211a22a8
dev-owen <jgyuity1289@gmail.com>
2023-04-11 22:14:43 +0900

    1-1 react vs vanilla js differences

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # javascript-es6-fundamental
 
 hello javascript
+dddd
---
+++ react-vs-vanillajs/react/src/App.js
@@ -0,0 +1,12 @@
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div>
+      <h1>Todo List (React)</h1>
+      <Todo text='리액트 배우기' />
+    </div>
+  );
+}
+
+export default App;
