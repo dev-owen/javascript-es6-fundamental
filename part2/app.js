5e34822a6c8585789d3bca92f6e79fd76009efa4
dev-owen <jgyuity1289@gmail.com>
2023-04-11 23:50:33 +0900

    1-3 part2 let const var end

--- part2/app.js
+++ part2/app.js
@@ -1,5 +1,15 @@
-// let (ES6)
+// let (ES6), block scope
+let age = 30;
+age = 31;
+
+let defaultAge;
 
 // const (ES6)
+const birthYear = 1990;
+birthYear = 1991;
 
-// var (ES5)
+const defaultBirthYear;
+
+// var (ES5), function scope
+var address = 'Seoul';
+address = 'Busan';
