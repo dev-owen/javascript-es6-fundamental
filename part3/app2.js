92b0c7c35384308eaf9356643cd27bfd78bae0e4
dev-owen <jgyuity1289@gmail.com>
2023-04-12 01:55:37 +0900

    1-4 part3 function declaration and expression end

--- part3/app2.js
+++ part3/app2.js
@@ -1,4 +1,19 @@
 // Function Declaration
 
+calcAge1(1990); // 가능
+
+function calcAge1(birthYear) {
+	return 2023 - birthYear;
+}
+
+console.log(calcAge1(1990));
 
 // Function Expression
+
+calcAge2(1992); // 불가능
+
+const calcAge2 = function (birthYear) {
+	return 2023 - birthYear;
+};
+
+console.log(calcAge2(1992));
