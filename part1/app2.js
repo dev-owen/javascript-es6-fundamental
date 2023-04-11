75baf795d5a9b5b97aace2f88f40436546425ee6
dev-owen <jgyuity1289@gmail.com>
2023-04-11 23:24:06 +0900

    1-2 part1 data type end

--- part1/app2.js
+++ part1/app2.js
@@ -3,15 +3,39 @@
 // Primitive
 
 // 1. Number
+console.log(10);
+let number = 12;
+console.log(number);
+console.log(typeof number);
 
 // 2. String
+console.log("Kevin");
+let name = "Kevin";
+console.log(name);
+console.log(typeof name);
 
 // 3. Boolean
+console.log(true);
+let isFun = true;
+console.log(isFun);
+console.log(typeof isFun);
 
 // 4. Undefined
+let apple;
+console.log(apple);
+console.log(typeof apple);
 
 // 5. Null
+let time = null;
+console.log(time);
+console.log(typeof time);
 
 // 6. Symbol
+let symbol = Symbol();
+console.log(symbol);
+console.log(typeof symbol);
 
 // 7. BigInt
+let theBiggestInt = 9007199254740991n;
+console.log(theBiggestInt);
+console.log(typeof BigInt);
